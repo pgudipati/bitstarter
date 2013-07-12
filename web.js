24b1685255fe60cdc327860f2a85e946bcbad1da
Prabhakar Gudipati <prabhakarreddy.gudipati@gmail.com>
2013-07-12 01:52:45 +0000

    updated fileread to web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
+var buf = new Buffer(fs.readFileSync('index.html','utf8'));
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World...');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
